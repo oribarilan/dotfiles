7f06009ea25bc137f8b6b5f7fdefa5d08ad90de0
ori <oribarilan@gmail.com>
2025-08-10 12:16:05 +0300

    theme and readable char under cursor

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,27 +1,21 @@
-local subtle_color = '#8c98b3' -- default comment and line numbers for tokyo-nights is too faded
-
 return {
   'folke/tokyonight.nvim',
-  priority = 1000, -- Make sure to load this before all the other start plugins.
-  config = function()
-    ---@diagnostic disable-next-line: missing-fields
-    require('tokyonight').setup {
-      on_colors = function(colors)
-        colors.comment = subtle_color
-      end,
-      on_highlights = function(hl, colors)
-        hl.LineNr = { fg = subtle_color }
-        hl.LineNrAbove = { fg = subtle_color }
-        hl.LineNrBelow = { fg = subtle_color }
-
-        -- Make Visual mode stand out more
-        hl.Visual = { bg = '#445b9b' } -- You can tweak this hex color
-      end,
-      styles = {
-        comments = { italic = false }, -- copilot ghost text will use italic, to diffrentiate it from comments
-      },
-    }
-
-    vim.cmd.colorscheme 'tokyonight-night'
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = 'night',
+    cache = false, -- avoid stale results while iterating
+    styles = { comments = { italic = false } },
+    on_highlights = function(hl, c)
+      local subtle = '#8c98b3'  -- subtle color for comments and line numbers
+      hl.Comment      = { fg = subtle, italic = false }
+      hl.LineNr       = { fg = subtle }
+      hl.LineNrAbove  = { fg = subtle }
+      hl.LineNrBelow  = { fg = subtle }
+      hl.Visual       = { bg = '#445b9b' }
+    end,
+  },
+  init = function()
+    vim.cmd.colorscheme('tokyonight-night')
   end,
 }
