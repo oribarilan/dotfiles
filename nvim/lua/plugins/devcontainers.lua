f6684ed80fa9a37ea019f5cbbd27285952e21eb5
ori <oribarilan@gmail.com>
2025-09-07 21:21:18 +0300

    remove avante

---
+++ nvim/lua/plugins/devcontainers.lua
@@ -0,0 +1,23 @@
+return {
+
+  -- dev
+  dir = '~/repos/personal/devcontainers.nvim',
+  dev = true, -- Enables development mode
+  -- event = 'LspAttach',
+  config = function()
+    require('devcontainers').setup {
+      dev_path = '~/repos/personal/devcontainers.nvim',
+      enter_mode = 'nested',
+    }
+  end,
+
+  -- prod
+  -- 'oribarilan/devcontainers.nvim',
+  -- -- tag = '1.0.0', -- or: branch = 'release/1.x' for latest non-breaking updates
+  -- -- event = 'LspAttach',
+  -- config = function()
+  --   require("devcontainers").setup{
+  --     enter_mode = 'nested',
+  --   }
+  -- end,
+}
--- nvim/lua/plugins/lensline.lua
+++ nvim/lua/plugins/lensline.lua
@@ -1,24 +1,29 @@
 return {
 
   -- dev
-  dir = '~/repos/personal/lensline.nvim',
-  dev = true, -- Enables development mode
+  -- dir = '~/repos/personal/lensline.nvim',
+  -- dev = true, -- Enables development mode
+  -- event = 'LspAttach',
+  -- config = function()
+  --   require('lensline').setup {
+  --     style = {
+  --       placement = 'inline',
+  --       prefix = '',
+  --     },
+  --     debug_mode = true,
+  --   }
+  -- end,
+
+  -- prod
+  'oribarilan/lensline.nvim',
+  branch = 'release/1.x',
   event = 'LspAttach',
   config = function()
-    require('lensline').setup {
+    require("lensline").setup {
       style = {
         placement = 'inline',
         prefix = '',
       },
-      debug_mode = true,
     }
   end,
-
-  -- prod
-  -- 'oribarilan/lensline.nvim',
-  -- tag = '1.0.0', -- or: branch = 'release/1.x' for latest non-breaking updates
-  -- event = 'LspAttach',
-  -- config = function()
-  --   require("lensline").setup()
-  -- end,
 }
