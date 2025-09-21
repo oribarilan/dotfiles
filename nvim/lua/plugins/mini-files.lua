a97e7d96cb072628ed0aefea7154148875b51db0
ori <oribarilan@gmail.com>
2025-09-21 17:36:13 +0300

    use mini.files as default explorer (on startup) instead of telescope

--- nvim/lua/plugins/mini-files.lua
+++ nvim/lua/plugins/mini-files.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require('mini.files').setup {
       options = {
-        use_as_default_explorer = false,
+        use_as_default_explorer = true,
       },
     }
 
