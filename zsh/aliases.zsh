78ae6f2fcaa6937551a5036cc969c52ad7aa6282
ori <oribarilan@gmail.com>
2025-09-26 23:07:07 +0300

    back nav aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,9 +5,15 @@ alias tattach='tmux attach-session -t "$(tmux ls | awk -F: '\''!($1 ~ /^[0-9]+$/
 alias t='tattach'
 alias y='yazi'
 
+# Quick directory back navigation aliases
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+
 # # zoxide jump with fuzzy matching of path 
 # alias z='zoxide jump'
-
 # cd into any folder with zoxide and fzf
 unalias zi 2>/dev/null
 zi() {
