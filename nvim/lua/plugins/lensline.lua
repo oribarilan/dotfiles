6183cf577f1be6817199b8e84d5bca005e43a0b9
ori <oribarilan@gmail.com>
2025-10-08 15:32:25 +0300

    lensline update and sesh config

--- nvim/lua/plugins/lensline.lua
+++ nvim/lua/plugins/lensline.lua
@@ -1,43 +1,59 @@
 return {
 
   -- dev multi
-  dir = '~/repos/personal/lensline.nvim',
-  dev = true, -- Enables development mode
+  -- dir = '~/repos/personal/lensline.nvim',
+  -- dev = true, -- Enables development mode
+  -- event = 'LspAttach',
+  -- config = function()
+  --   require('lensline').setup {
+  --     profiles = {
+  --       {
+  --         name = 'default',
+  --         providers = {
+  --           {
+  --             name = 'usages',
+  --             include = { 'refs', 'defs', 'impls' },
+  --             breakdown = true,
+  --           },
+  --           { name = 'last_author' },
+  --         },
+  --         style = {
+  --           placement = 'inline',
+  --           prefix = '',
+  --         },
+  --       },
+  --       {
+  --         name = 'short',
+  --         providers = {
+  --           {
+  --             name = 'usages',
+  --             include = { 'refs', 'defs', 'impls' },
+  --             breakdown = false,
+  --           },
+  --           { name = 'last_author' },
+  --         },
+  --       },
+  --     },
+  --     debug_mode = true,
+  --   }
+  -- end,
+
+  -- prod
+  'oribarilan/lensline.nvim',
+  branch = 'release/2.x',
   event = 'LspAttach',
   config = function()
     require('lensline').setup {
       profiles = {
         {
           name = 'minimal',
-          providers = { { name = 'references' } },
           style = {
             placement = 'inline',
             prefix = '',
-          },
-        },
-        {
-          name = 'detailed',
-          providers = { { name = 'last_author' } , { name = 'references' } },
-          style = {
-            placement = 'above',
           },
         },
       },
       debug_mode = true,
     }
   end,
-
-  -- prod
-  -- 'oribarilan/lensline.nvim',
-  -- branch = 'release/1.x',
-  -- event = 'LspAttach',
-  -- config = function()
-  --   require('lensline').setup {
-  --     style = {
-  --       placement = 'inline',
-  --       prefix = '',
-  --     },
-  --     debug_mode = true,
-  --   }
-  -- end,
 }
