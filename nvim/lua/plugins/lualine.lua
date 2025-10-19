b72aa7a6efe258fc12f3120fda118352aca3cbc0
ori <oribarilan@gmail.com>
2025-10-19 21:42:48 +0300

    added modified file count to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -6,6 +6,16 @@ return {
       return rec ~= '' and 'Recording @' .. rec or ''
     end
 
+    local function unsaved_changes()
+      local modified_count = 0
+      for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+        if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].modified then
+          modified_count = modified_count + 1
+        end
+      end
+      return modified_count > 0 and '[' .. modified_count .. '+]' or ''
+    end
+
     require('lualine').setup {
       options = {
         theme = 'tokyonight-night',
@@ -15,10 +25,22 @@ return {
           'filename',
           recording_macro,
         },
+        lualine_x = {
+          unsaved_changes,
+          'encoding',
+          'fileformat',
+          'filetype',
+        },
       },
     }
 
     vim.api.nvim_create_autocmd({ 'RecordingEnter', 'RecordingLeave' }, {
+      callback = function()
+        require('lualine').refresh()
+      end,
+    })
+
+    vim.api.nvim_create_autocmd({ 'BufModifiedSet' }, {
       callback = function()
         require('lualine').refresh()
       end,
