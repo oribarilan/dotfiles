db06e8784a6e3dfc5e6f299774959645b42a2b70
ori <oribarilan@gmail.com>
2025-10-24 16:13:31 +0300

    enabling powerlevel10k instant prompt (top of zshrc)

--- zsh/theme.zsh
+++ zsh/theme.zsh
@@ -1,15 +1,15 @@
-# cache brew prefix to avoid slow subprocess calls (if not already set)
-HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
-
-# if p10k installed via homebrew, follow the installation
-source $HOMEBREW_PREFIX/share/powerlevel10k/powerlevel10k.zsh-theme
-
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Enable Powerlevel10k instant prompt. Should stay at the very top of zsh config.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
+
+# cache brew prefix to avoid slow subprocess calls (if not already set)
+HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
+
+# if p10k installed via homebrew, follow the installation
+source $HOMEBREW_PREFIX/share/powerlevel10k/powerlevel10k.zsh-theme
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
