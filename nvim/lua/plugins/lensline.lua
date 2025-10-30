3010e1ccbef5749eaeddd0124c634d36d7610d79
ori <oribarilan@gmail.com>
2025-10-30 18:09:25 +0200

    update sidekick + lensline conf

--- nvim/lua/plugins/lensline.lua
+++ nvim/lua/plugins/lensline.lua
@@ -1,46 +1,8 @@
 return {
 
   -- dev multi
-  -- dir = '~/repos/personal/lensline.nvim',
-  -- dev = true, -- Enables development mode
-  -- event = 'LspAttach',
-  -- config = function()
-  --   require('lensline').setup {
-  --     profiles = {
-  --       {
-  --         name = 'default',
-  --         providers = {
-  --           {
-  --             name = 'usages',
-  --             include = { 'refs', 'defs', 'impls' },
-  --             breakdown = true,
-  --           },
-  --           { name = 'last_author' },
-  --         },
-  --         style = {
-  --           placement = 'inline',
-  --           prefix = '',
-  --         },
-  --       },
-  --       {
-  --         name = 'short',
-  --         providers = {
-  --           {
-  --             name = 'usages',
-  --             include = { 'refs', 'defs', 'impls' },
-  --             breakdown = false,
-  --           },
-  --           { name = 'last_author' },
-  --         },
-  --       },
-  --     },
-  --     debug_mode = true,
-  --   }
-  -- end,
-
-  -- prod
-  'oribarilan/lensline.nvim',
-  branch = 'release/2.x',
+  dir = '~/repos/personal/lensline.nvim',
+  dev = true, -- Enables development mode
   event = 'LspAttach',
   config = function()
     require('lensline').setup {
@@ -54,6 +16,28 @@ return {
         },
       },
       debug_mode = true,
+      limits = {
+        exclude = {}, -- use gitignore instead
+      }
     }
   end,
+
+  -- prod
+  -- 'oribarilan/lensline.nvim',
+  -- branch = 'release/2.x',
+  -- event = 'LspAttach',
+  -- config = function()
+  --   require('lensline').setup {
+  --     profiles = {
+  --       {
+  --         name = 'minimal',
+  --         style = {
+  --           placement = 'inline',
+  --           prefix = '',
+  --         },
+  --       },
+  --     },
+  --     debug_mode = true,
+  --   }
+  -- end,
 }
