4c953a8b483c6ab537b98dfd5727d6deb68bc4c0
ori <oribarilan@gmail.com>
2025-11-13 20:01:01 +0200

    buffer close with move to a different open buffer

--- nvim/lua/scripts/utility_commands.lua
+++ nvim/lua/scripts/utility_commands.lua
@@ -16,9 +16,26 @@ end
 
 vim.api.nvim_create_user_command('LineNumbersToggle', LineNumbersToggle, { desc = 'Toggle between absolute and relative line numbers' })
 
+-- Close current buffer and switch to another or create empty buffer
+local function close_buffer()
+  local current_buf = vim.api.nvim_get_current_buf()
+  local buffers = vim.fn.getbufinfo { buflisted = 1 }
+  
+  -- If this is the only buffer, create a new empty one first
+  if #buffers <= 1 then
+    vim.cmd 'enew'
+  else
+    -- Switch to alternate buffer or next buffer
+    vim.cmd 'bp'
+  end
+  
+  -- Delete the original buffer
+  vim.api.nvim_buf_delete(current_buf, { force = false })
+end
+
 -- Keymaps for utility group (<leader>u)
 vim.keymap.set('n', '<leader>ul', '<cmd>LineNumbersToggle<cr>', { desc = '[L]ine numbers toggle' })
 vim.keymap.set('n', '<leader>ur', '<cmd>e!<cr>', { desc = '[R]eload file from disk' })
-vim.keymap.set('n', '<leader>ub', '<cmd>bd<cr>', { desc = '[B]uffer close' })
+vim.keymap.set('n', '<leader>ub', close_buffer, { desc = '[B]uffer close' })
 vim.keymap.set('n', '<leader>uce', '<cmd>CopilotEnable<cr>', { desc = '[C]opilot [E]enable' })
 vim.keymap.set('n', '<leader>ucd', '<cmd>CopilotDisable<cr>', { desc = '[C]opilot [D]isable' })
